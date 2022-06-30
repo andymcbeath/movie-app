d177163a9b3fc02dc20be644ce566b6801e80b03
andymcbeath <mcbeath1@gmail.com>
2022-06-30 18:12:52 -0500

    adds seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,7 @@
 actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
 actor.save
+
+actor2 = Actor.new({ first_name: "Tom", last_name: "Cruise", known_for: "TopGun" })
+actor2.save
+
+actor3 = Actor.new ({ first_name: "Jennifer", last_name: "Aniston", known_for: "Office_Space" })
